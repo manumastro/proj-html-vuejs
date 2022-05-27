bad2a8739ba6344e58d535efa51847bed3c752d2
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-05-27 14:20:56 +0200

    header stilizzato

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-    
+    <HeaderC />
   </div>
 </template>
 
 <script>
+import HeaderC from './components/HeaderC.vue'
 
 export default {
   name: 'App',
   components: {
+    HeaderC
     
   }
 }
 </script>
 
 <style lang="scss">
+@import './assets/style/_general.scss';
+@import './assets/style/_vars.scss';
 
 </style>
---
+++ src/components/HeaderC.vue
@@ -0,0 +1,74 @@
+<template>
+  <header>
+    <div class="container d-flex h-100">
+      <div class="logo h-100 d-flex align-items-center justify-content-center">
+        <a href="#">
+          <img src="../assets/images/logo.png" alt="THE KEYNOTE">
+        </a>
+      </div>
+      <nav class="navbar w-100 mt-2">
+        <ul class="d-flex m-0">
+          <li><a class="active" href="#">home</a></li>
+          <li><a href="#">pages</a></li>
+          <li><a href="#">program</a></li>
+          <li><a href="#">tickets</a></li>
+          <li><a href="#">speakers</a></li>
+          <li><a href="#">papers</a></li>
+          <li><a href="#">blog</a></li>
+          <li><a href="#">shortcodes</a></li>
+          <li class="pe-4"><a href="#">|</a></li>
+          <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>    
+        </ul>    
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderC'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/_vars.scss';
+
+header{
+  height: 110px;
+  background-color: $white;
+  .container{
+    .logo{
+      padding: 0 90px;
+    }
+  
+    nav{
+      ul{
+        padding: 0 35px;
+      }
+      li{
+        padding-right: 40px;
+      }
+      a{
+        position: relative;
+        color: $gray;
+        text-transform: uppercase;
+        font-size: 12px;
+        font-weight: 600;
+        &.active{
+          color: $scarlet;
+        }
+        &.active::before{
+          content: '';
+          position: absolute;
+          bottom: -10px;
+          right: 0;
+          height: 3px;
+          width: 100%;
+          background-color: $scarlet;
+        }
+      }
+      
+    }
+  }
+}
+</style>
